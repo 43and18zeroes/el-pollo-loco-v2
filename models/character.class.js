4fc5fcf909a016172265018875e52beff2777580
Christoph <c.wagner.germany@gmail.com>
2022-05-12 20:13:01 +0200

    adjusted images ratios

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,9 @@
 class Character extends MovableObject {
 
+    y = 180;
+    width = 120;
+    height = 250;
+
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,8 +1,8 @@
 class Chicken extends MovableObject {
 
+    y = 345;
     width = 80;
     height = 80;
-    y = 345;
 
     constructor() {
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
