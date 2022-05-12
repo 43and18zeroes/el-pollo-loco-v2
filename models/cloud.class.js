6051d42b0cf2d2dec899487e440db806b57c83d7
Christoph <c.wagner.germany@gmail.com>
2022-05-12 20:45:53 +0200

    added cloud motion

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -5,7 +5,15 @@ class Cloud extends MovableObject {
 
     constructor() {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
+
         this.x = Math.floor(Math.random() * 500);
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60); // 60 FPS
     }
 
 }
