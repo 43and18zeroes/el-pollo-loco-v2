716e32466238820f91c8eb4274c5ad9b8b3b9133
Christoph <c.wagner.germany@gmail.com>
2022-05-12 19:42:32 +0200

    added math to bg

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,9 @@
 class Chicken extends MovableObject {
 
+    width = 80;
+    height = 80;
+    y = 345;
+
     constructor() {
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
         this.x = 200 + Math.floor(Math.random() * 500);
--- models/world.class.js
+++ models/world.class.js
@@ -10,6 +10,8 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
         new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0)
     ];
 
