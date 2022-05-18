eeca4b9f63420131680ae059b7983988532330bf
Christoph <c.wagner.germany@gmail.com>
2022-05-18 18:41:08 +0200

    multiple img added

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -1,14 +1,29 @@
 class MovableObject {
+    img;
     x = 120;
     y = 180;
     width = 120;
     height = 250;
-    img;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path; // local
+            this.imageCache[path] = path; // global
+        });
+    }
+
 
     moveRight() {
         console.log('Moving right'); // this.img = document.getElementbyID('image') <img id="image">
