ff98bf742a37d08cf04966edd5ab4e407d86735d
Christoph <c.wagner.germany@gmail.com>
2022-07-13 19:45:42 +0200

    chicken number and spawn range

--- levels/level1.js
+++ levels/level1.js
@@ -5,6 +5,10 @@ function initLevel() {
             new Chicken(),
             new Chicken(),
             new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
             new Endboss()
         ],
         [
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -13,7 +13,7 @@ class Chicken extends MovableObject {
 
     constructor() {
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
-        this.x = 200 + Math.floor(Math.random() * 500);
+        this.x = 400 + Math.floor(Math.random() * 1500);
 
         this.loadImages(this.IMAGES_WALKING);
         this.moveLeft();
