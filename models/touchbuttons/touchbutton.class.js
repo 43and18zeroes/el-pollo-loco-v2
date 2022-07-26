49b21183b6326437077b406baf091a2eb1f36407
Christoph <c.wagner.germany@gmail.com>
2022-07-26 23:39:58 +0200

    second touch button responsive

--- models/touchbuttons/touchbutton.class.js
+++ models/touchbuttons/touchbutton.class.js
@@ -1,10 +1,12 @@
 class TouchButton extends DrawableObject {
+    canvasHeight = document.getElementById('canvas').clientHeight;
+    touchButtonSize = 100;
 
     constructor() {
         super();
         this.y = 380;
-        this.width = 100;
-        this.height = 100;
+        this.width = this.touchButtonSize;
+        this.height = this.touchButtonSize;
     }
 
 }
