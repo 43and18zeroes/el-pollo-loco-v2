28040853a7be1c36ddda96999f30fe55c6e3f541
Christoph <c.wagner.germany@gmail.com>
2022-08-11 18:33:08 +0200

    coin spawn height

--- models/coin.class.js
+++ models/coin.class.js
@@ -13,7 +13,7 @@ class Coin extends CollectableObject {
     constructor() {
         super().loadImage('img/8.Coin/Moneda1.png');
         this.x = 400 + Math.floor(Math.random() * 10 * 180);
-        this.y = 50 + Math.floor(Math.random() * 10 * 30);
+        this.y = 100 + Math.floor(Math.random() * 10 * 25);
     }
 
 }
