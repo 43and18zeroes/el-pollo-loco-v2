7886ff81e7b346217a184ae1ca9ac0b7ed89f83c
Christoph <c.wagner.germany@gmail.com>
2022-08-25 19:34:49 +0200

    bottle rotation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -7,13 +7,22 @@ class ThrowableObject extends MovableObject {
         right: 5
     }
 
+    IMAGES_ROTATING = [
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 3.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 4.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 5.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 6.png'
+    ];
+
     constructor(x, y) {
-        super().loadImage('img/7.Marcadores/Icono/Botella.png');
+        super().loadImage('img/6.botella/Rotación/Mesa de trabajo 1 copia 3.png');
+        this.loadImages(this.IMAGES_ROTATING);
         this.x = x;
         this.y = y;
         this.width = 60;
         this.height = 60;
         this.throw();
+        this.rotationAnimation();
     }
 
     throw() {
@@ -22,5 +31,11 @@ class ThrowableObject extends MovableObject {
         setInterval(() => {
             this.x += 15;
         }, 25);
+    }
+
+    rotationAnimation() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_ROTATING);
+        }, 100);
     }
 }
